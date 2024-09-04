029f6340dfb1189b4d333d4d8fbff7edca88e204
jep <vmuhammadfarhan@gmail.com>
2024-09-04 14:33:42 +0700

    Tutorial 1 Selesai, tadi lupa ganti nama

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306231422',
+        'name': 'Muhammad Farhan Ramadhan',
+        'class': 'PBP A'
     }
 
     return render(request, "main.html", context)
